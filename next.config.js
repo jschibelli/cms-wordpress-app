a495f30d9236b6ff3a3065cf5b90b43e8d643b61
John Schibelli <jschibelli@gmail.com>
2024-01-16 22:35:37 -0500

    initial project commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+// next.config.js
+
+// Validate WordPress API URL
 if (!URL.canParse(process.env.WORDPRESS_API_URL)) {
   throw new Error(`
     Please provide a valid WordPress instance URL.
@@ -5,8 +8,9 @@ if (!URL.canParse(process.env.WORDPRESS_API_URL)) {
   `);
 }
 
+// Parse components of the WordPress API URL
 const { protocol, hostname, port, pathname } = new URL(
-  process.env.WORDPRESS_API_URL,
+  process.env.WORDPRESS_API_URL
 );
 
 /** @type {import('next').NextConfig} */
@@ -14,11 +18,12 @@ module.exports = {
   images: {
     remotePatterns: [
       {
-        protocol: protocol.slice(0, -1),
-        hostname,
-        port,
-        pathname: `${pathname}/**`,
+        protocol: protocol.slice(0, -1), // Remove the trailing colon from the protocol
+        hostname, // Hostname from the WordPress API URL
+        port, // Port from the WordPress API URL
+        pathname: `${pathname}/**`, // Pathname from the WordPress API URL, allowing all paths
       },
     ],
+    domains: ["schibelli.com", "secure.gravatar.com"], // Add 'schibelli.com' to the allowed domains for image loading
   },
 };
